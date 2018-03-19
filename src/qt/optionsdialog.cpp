8416abb80eebe5a5ffbd5ac5c4a6b7e7a7534944
Ivan Zagorulko <ivan.zagorulko@gmail.com>
2018-03-19 16:46:31 +0200

    Rename DASH to EZIOCOIN

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -92,11 +92,11 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
     }
     
     /* Theme selector */
-    ui->theme->addItem(QString("DASH-light"), QVariant("light"));
-    ui->theme->addItem(QString("DASH-light-hires"), QVariant("light-hires"));
-    ui->theme->addItem(QString("DASH-blue"), QVariant("drkblue"));
-    ui->theme->addItem(QString("DASH-Crownium"), QVariant("crownium"));
-    ui->theme->addItem(QString("DASH-traditional"), QVariant("trad"));
+    ui->theme->addItem(QString("EZIOCOIN-light"), QVariant("light"));
+    ui->theme->addItem(QString("EZIOCOIN-light-hires"), QVariant("light-hires"));
+    ui->theme->addItem(QString("EZIOCOIN-blue"), QVariant("drkblue"));
+    ui->theme->addItem(QString("EZIOCOIN-Crownium"), QVariant("crownium"));
+    ui->theme->addItem(QString("EZIOCOIN-traditional"), QVariant("trad"));
     
     /* Language selector */
     QDir translations(":translations");
